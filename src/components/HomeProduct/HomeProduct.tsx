3a069bce22b1873e86e0c86638458852b1eaa88d
Amir Hossein Kheirollahpour <amir1.kh2006@gmail.com>
2025-04-05 11:04:49 +0330

    some changes in Responsive

--- src/components/HomeProduct/HomeProduct.tsx
+++ src/components/HomeProduct/HomeProduct.tsx
@@ -1,33 +1,60 @@
-import { Tab , Tabs , Box } from "@mui/material";
+import { Tab, Tabs, Box } from "@mui/material";
 import HomeCard from "./HomeCardProducts";
 
-export default function HomeProduct(){
+export default function HomeProduct() {
   // const apifunction = () =>{
-    
+
   // }
-    return(
+  return (
     <>
-    <div className=" hidden sm:flex flex-wrap mt-12 flex-col items-start  px-40 py-14 h-[1632px]  md:h-[1056px] ">
+      <div className=" hidden sm:flex flex-wrap mt-12 flex-col items-start  px-40 py-14 h-[1632px]  md:h-[1056px] ">
         <div className="md:flex hidden">
-    <Box  sx={{ borderBottom: 1, borderColor: 'divider' }}>
-  <Tabs aria-label="basic tabs example">
-    <Tab label="New Arraival" />
-    <Tab label="Best Seller" />
-    <Tab label="Featured Products" />
-  </Tabs>
-</Box>
+          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+            <Tabs aria-label="basic tabs example">
+              <Tab label="New Arraival" />
+              <Tab label="Best Seller" />
+              <Tab label="Featured Products" />
+            </Tabs>
+          </Box>
         </div>
-<div className="flex flex-wrap gap-5 w-full h-[1632px] md:h-[880px] pt-6">
-    <HomeCard img="" name="" price=""/>
-    <HomeCard img="" name="" price=""/>
-    <HomeCard img="" name="" price=""/>
-    <HomeCard img="" name="" price=""/>
-    <HomeCard img="" name="" price=""/>
-    <HomeCard img="" name="" price=""/>
-    <HomeCard img="" name="" price=""/>
-    <HomeCard img="" name="" price=""/>
-</div>
-    </div>
+        <div className="flex flex-wrap gap-5 w-full h-[1632px] md:h-[880px] pt-6">
+          <HomeCard img="" name="" price="" />
+          <HomeCard img="" name="" price="" />
+          <HomeCard img="" name="" price="" />
+          <HomeCard img="" name="" price="" />
+          <HomeCard img="" name="" price="" />
+          <HomeCard img="" name="" price="" />
+          <HomeCard img="" name="" price="" />
+          <HomeCard img="" name="" price="" />
+        </div>
+      </div>
+      <div className=" sm:hidden flex-col gap-y-4 flex w-full h-[1632px] px-4 py-14">
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <Tabs aria-label="basic tabs example">
+            <Tab label="New Arraival" />
+            <Tab label="Best Seller" />
+            <Tab label="Featured Products" />
+          </Tabs>
+        </Box>
+        <div>
+          <div className="flex mb-4 gap-x-6 justify-center">
+            <HomeCard img="" name="" price=""/>
+            <HomeCard img="" name="" price=""/>
+          </div>
+          <div className="flex mb-4 gap-x-6 justify-center">
+            <HomeCard img="" name="" price=""/>
+            <HomeCard img="" name="" price=""/>
+          </div>
+          <div className="flex mb-4 gap-x-6 justify-center">
+            <HomeCard img="" name="" price=""/>
+            <HomeCard img="" name="" price=""/>
+          </div>
+          <div className="flex gap-x-6 justify-center">
+            <HomeCard img="" name="" price=""/>
+            <HomeCard img="" name="" price=""/>
+          </div>
+        </div>
+      </div>
     </>
-)
+  );
 }
--- src/components/PopularProduct/CardPopular.tsx
+++ src/components/PopularProduct/CardPopular.tsx
@@ -9,10 +9,10 @@ interface Card {
 export default function CardPopular(prop:Card){
     return(
         <>
-        <div className={`sm:w-1/4 h-[640px] w-[375px]  flex flex-col items-center justify-center  ${prop.bgcolor}`}>    
+        <div className={`sm:w-1/4 h-[640px]  w-[475px]  sm:ml-0 pr-6   flex flex-col sm:items-center items-start justify-center  ${prop.bgcolor}`}>    
             <img src={prop.img} alt="" />
             <br />
-            <div className={`w-[296px] ${prop.contstyle} h-[208px] flex flex-col items-center`}>
+            <div className={`w-[296px] ${prop.contstyle} h-[208px] sm:ml-0 ml-12 flex flex-col items-center`}>
             <h2 className={`${prop.nameClass}`} >{prop.name}</h2>
             <p className="text-sm  pb-5 text-[#909090] font-medium">iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of use.</p>
             <div className="w-full">
