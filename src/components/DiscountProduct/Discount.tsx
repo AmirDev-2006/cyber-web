9d8490ecf412240772b044e9d16881240465bcd5
Amir Hossein Kheirollahpour <amir1.kh2006@gmail.com>
2025-04-05 14:28:16 +0330

    some changes

--- src/components/DiscountProduct/Discount.tsx
+++ src/components/DiscountProduct/Discount.tsx
@@ -6,10 +6,10 @@ export default function Discount(){
         <div className="w-full flex-col items-center sm:items-start gap-y-8 h-[896px] flex sm:h-[656px] py-14 px-4 sm:px-40 sm:py-20">
             <h2 className="text-[24px] font-medium">Discounts up to -50%</h2>
             <div className="flex gap-x-4 gap-y-4 justify-center sm:justify-start flex-wrap w-full">
-                <HomeCard/>
-                <HomeCard/>
-                <HomeCard/>
-                <HomeCard/>
+                <HomeCard name="" img="" price=""/>
+                <HomeCard name="" img="" price=""/>
+                <HomeCard name="" img="" price=""/>
+                <HomeCard name="" img="" price=""/>
             </div>
         </div>
         </>
